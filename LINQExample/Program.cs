9643482d25eb2bce2198827dddfb53d97569324b
Home <Home@LAPTOP-2E6V6ACM>
2023-11-05 20:38:55 +0200

    .

--- LINQExample/Program.cs
+++ LINQExample/Program.cs
@@ -1,15 +1,17 @@
 ﻿using LINQExample.Extentions;
 using LINQExample.Models;
+using LINQ;
 
 namespace LINQExample
 {
     internal class Program
     {
 
-        
+
         static void Main(string[] args)
         {
+            List<Student> students = new List<Student>();
+            students = Student.FillList();
            
-        }
     }
 }
